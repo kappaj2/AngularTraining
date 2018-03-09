2add2eb299aee3f41732c838a52f78b5146e49d2
Andre <kappaj@gmail.com>
2018-03-09 21:34:21 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: 'Helloooow ng2'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app p2';
 }
